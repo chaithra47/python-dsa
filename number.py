f6c23db4209f98d8fb229cdb2f99520a0779485e
chaithra47 <cbangera71@gmail.com>
2025-07-26 20:21:14 +0530

    deleted file 1

--- number.py
+++
@@ -1,8 +0,0 @@
-number = [1,2,3,4,5]
-print(" the first element:",number[0])
-print(" the last element:",number[-1])
-print("all element using loop",number)
-for num in  (number):
-    print(num,end='') 
-print ("reversed list using slice")
-print(number[::-1]) 
