e8bec2542f5f97217e91297ade8d1cff5c6e821b
chaithra47 <cbangera71@gmail.com>
2025-07-27 16:49:52 +0530

    Update 6th prg.py

--- 6th prg.py
+++ 6th prg.py
@@ -3,6 +3,5 @@ for i in range(3):
     name = input("enter name of student {i + 1}")
     marks = int(input("enter marks of student {i + 1}"))
     student[name] = marks
-Topper = max(student, key = student.get)
-print("topper is", topper)
-
+Topper = max(student, key=student.get)
+print("topper is", Topper)
