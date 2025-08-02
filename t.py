8506b6fa7a8478b63f44a71e8dd743a080025961
chaithra47 <cbangera71@gmail.com>
2025-08-02 09:38:52 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,14 @@
+import time 
+
+def linear_time(n):
+    print(f"\n0(n) for n = {n}")
+    start= time.time()
+    for i in range(n):
+        pass # simulate some work
+    end = time.time()
+    print(f"Time taken: {end - start:.6f} seconds")
+
+linear_time(10000)
+linear_time(20000)
+linear_time(40000)
+
